f68a4bed82499b406c8293471abf91e4970e5590
marcosav24 <marcosav24@gmail.com>
2024-10-25 19:07:01 +0000

    actualiza contador de palabras

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = "frankenstein.txt"
     text = get_book_text(book_path)
+    word_count = count_words(text)
+    print(f"Word count: {word_count}")
     print(text)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def count_words(text):
+    words = text.split()
+    return len(words)
 
 main()
