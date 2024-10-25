c566ea2537dbaf8d654a448ad24cdb077935a547
marcosav24 <marcosav24@gmail.com>
2024-10-25 19:54:04 +0000

    actualiza contador de caracteres

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ def main():
     book_path = "frankenstein.txt"
     text = get_book_text(book_path)
     word_count = count_words(text)
+    charac_count = count_characters(text)
     print(f"Word count: {word_count}")
+    print(f"Character counts: {charac_count}")
     print(text)
+    
 
 def get_book_text(path):
     with open(path) as f:
@@ -13,4 +16,14 @@ def count_words(text):
     words = text.split()
     return len(words)
 
+def count_characters(text):
+    char_count = {}
+    for i in text.lower():
+        if i not in char_count:
+            char_count[i] = 1
+        else:
+            char_count[i] = char_count[i] + 1
+        
+    return char_count
+    
 main()
